c7b3b678ddc8f1edae622dcd4b3d41c1d05e00c0
Rick Cobb <cobbr2@yahoo.com>
2025-03-21 15:08:49 -0700

    get the link in the right place

--- sum.go
+++ sum.go
@@ -1,10 +1,9 @@
 // sum is a stubby module that adds some stuff
-// If you want real stuff, see [MathIsFun]
-//
-// [MathIsFun]: http://www.mathisfun.com/numbers/addition.html
 package sum
 
-// Add adds two numbers and returns their sum
+// Add adds two numbers and returns their sum. See [MathIsFun]
+//
+// [MathIsFun]: http://www.mathisfun.com/numbers/addition.html
 func Add(a,b int) int {
 	return a+b
 }
