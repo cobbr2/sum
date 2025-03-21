f71622782379cb64a5bbb53681a0d1f11605c833
Rick Cobb <cobbr2@yahoo.com>
2025-03-21 15:13:32 -0700

    Version 2: much larger API

---
+++ v2/sum.go
@@ -0,0 +1,16 @@
+// sum is a stubby module that adds some stuff
+package sum
+
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	Integer
+	Float
+}
+
+// Add adds two numbers and returns their sum. See [MathIsFun]
+//
+// [MathIsFun]: http://www.mathisfun.com/numbers/addition.html
+func Add[N Number](a, b N) N {
+	return a + b
+}
